7ee2c637c93a904b7eea1da1b4e77e1a42061ab6
jung.sammie <jung.sammie@gmail.com>
2021-03-10 16:02:11 +0900

    카드 작업중

---
+++ components/Card.js
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+import cardStyles from '../styles/Card.module.css';
+
+const Card = () => {
+  return (
+    <div className={cardStyles.card}>
+      <div className="row mb-1">
+        <span className={cardStyles.category}>디자인</span>
+      </div>
+      <h2 className={cardStyles.title}>Weekly D</h2>
+      <p className={`bodyText2 ${cardStyles.description}`}>
+        "주로 국내 디자이너가 쓴 글이나 디자인 관련 글을 수집합니다. 매주 수요일
+        오전 8시에 만나요"
+      </p>
+
+      <div className={cardStyles.tagSection}>
+        <h6 className={cardStyles.smallHeading}>발행 주기</h6>
+        <div className="mt-1">
+          <span className={cardStyles.sendingTermTag}>매주</span>
+        </div>
+        <div className="mt-1">
+          <span className={cardStyles.sendingDayTag}>일</span>
+          <span className={cardStyles.sendingDayTag}>월</span>
+          <span className={cardStyles.sendingDayTag}>화</span>
+          <span className={`${cardStyles.sendingDayTag} ${cardStyles.on}`}>
+            수
+          </span>
+          <span className={cardStyles.sendingDayTag}>목</span>
+          <span className={cardStyles.sendingDayTag}>금</span>
+          <span className={cardStyles.sendingDayTag}>토</span>
+        </div>
+
+        <div className="mt-3">
+          <h6 className={cardStyles.smallHeading}>관련 태그</h6>
+          <div className="mt-1 mb-3">
+            <span className={`caption mr-1 ${cardStyles.relatedTag}`}>
+              디자인
+            </span>
+            <span className={`caption mr-1 ${cardStyles.relatedTag}`}>
+              디자이너
+            </span>
+            <span className={`caption mr-1 ${cardStyles.relatedTag}`}>
+              아티클
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={cardStyles.subscriptionBtn}>
+        구독하기
+        <Image
+          src="/i_arrow-right.png"
+          alt="arrow"
+          width={24}
+          height={24}
+          layout="fixed"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,18 @@
 import Nav from "./Nav"
 import styles from '../styles/Layout.module.css'
+import Card from "./Card"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Nav />
       <div className={styles.container}>
-
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </>
   )
