52f88c07d16315cb5d4681af059793bcf951b32d
jung.sammie <jung.sammie@gmail.com>
2021-03-12 23:08:30 +0900

    카드 데이터 연동

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,61 @@
-import Image from 'next/image';
 import cardStyles from '../styles/Card.module.css';
+import { Icon, InlineIcon } from '@iconify/react';
+import arrowLine from '@iconify/icons-clarity/arrow-line';
 
 const Card = ({ newsletter }) => {
+  const categoryArr = [
+    { code: 'design', name: '디자인' },
+    { code: 'develop', name: '개발' },
+    { code: 'career', name: '커리어' },
+    { code: 'marketing', name: '마케팅' },
+    { code: 'sci', name: '과학과 기술' },
+    { code: 'lifestyle', name: '라이프스타일' },
+    { code: 'news', name: '뉴스' },
+    { code: 'travel', name: '여행' },
+    { code: 'society', name: '사회' },
+    { code: 'economy', name: '경제' },
+    { code: 'culture', name: '문화' },
+  ];
+
+  const sendingTermArr = [
+    { code: 'daily', name: '매일' },
+    { code: 'weekly', name: '매주' },
+    { code: '10 days', name: '10일' },
+    { code: 'biweekly', name: '격주' },
+    { code: 'monthly', name: '매달' },
+    { code: '?', name: '?' },
+  ];
+
+  const sendingDayArr = [
+    { code: 'Mon', name: '월' },
+    { code: 'Tue', name: '화' },
+    { code: 'Wed', name: '수' },
+    { code: 'Thu', name: '목' },
+    { code: 'Fri', name: '금' },
+    { code: 'Sat', name: '토' },
+    { code: 'Sun', name: '일' },
+  ];
+
+  let category;
+  categoryArr.forEach((item) => {
+    if (newsletter.categories === item.name) {
+      category = item.code;
+    }
+  });
+
+  let sendingTerm;
+  sendingTermArr.forEach((item) => {
+    if (item.code === newsletter.sendingTerm) {
+      sendingTerm = item.name;
+    }
+  });
+
   return (
     <div className={cardStyles.card}>
       <div className="row mb-8">
-        <span className={cardStyles.category}>{newsletter.categories}</span>
+        <span className={`${cardStyles.category} ${category}`}>
+          {newsletter.categories}
+        </span>
       </div>
       <h2 className={cardStyles.title}>{newsletter.title}</h2>
       <p className={`bodyText2 ${cardStyles.description}`}>
@@ -13,47 +63,65 @@ const Card = ({ newsletter }) => {
       </p>
 
       <div className={cardStyles.tagSection}>
-        <h6 className={cardStyles.smallHeading}>발행 주기</h6>
+        <h6 className="caption">발행 주기</h6>
         <div className="mt-8">
-          <span className={cardStyles.sendingTermTag}>매주</span>
+          <span className={cardStyles.sendingTermTag}>{sendingTerm}</span>
         </div>
         <div className="mt-8">
-          <span className={cardStyles.sendingDayTag}>일</span>
-          <span className={cardStyles.sendingDayTag}>월</span>
-          <span className={cardStyles.sendingDayTag}>화</span>
-          <span className={`${cardStyles.sendingDayTag} ${cardStyles.on}`}>
-            수
-          </span>
-          <span className={cardStyles.sendingDayTag}>목</span>
-          <span className={cardStyles.sendingDayTag}>금</span>
-          <span className={cardStyles.sendingDayTag}>토</span>
+          {newsletter.sendingTerm === 'daily'
+            ? sendingDayArr.map((day) => {
+                return (
+                  <span
+                    key={day.code}
+                    className={`${cardStyles.sendingDayTag} ${cardStyles.on}`}
+                  >
+                    {day.name}
+                  </span>
+                );
+              })
+            : sendingDayArr.map((day) => {
+                if (newsletter.sendingDay.includes(day.code)) {
+                  return (
+                    <span
+                      key={day.code}
+                      className={`${cardStyles.sendingDayTag} ${cardStyles.on}`}
+                    >
+                      {day.name}
+                    </span>
+                  );
+                }
+                return (
+                  <span key={day.code} className={cardStyles.sendingDayTag}>{day.name}</span>
+                );
+              })}
         </div>
 
         <div className="mt-24">
-          <h6 className={cardStyles.smallHeading}>관련 태그</h6>
+          <h6 className="caption">관련 태그</h6>
           <div className="mt-8 mb-24">
-            <span className={`caption mr-8 ${cardStyles.relatedTag}`}>
-              디자인
-            </span>
-            <span className={`caption mr-8 ${cardStyles.relatedTag}`}>
-              디자이너
-            </span>
-            <span className={`caption mr- ${cardStyles.relatedTag}`}>
-              아티클
-            </span>
+            {newsletter.tags.map((tag) => {
+              return (
+                <span
+                  key={tag}
+                  className={`caption mr-8 ${cardStyles.relatedTag}`}
+                >
+                  {tag}
+                </span>
+              );
+            })}
           </div>
         </div>
       </div>
-      <div className={cardStyles.subscriptionBtn}>
-        구독하기
-        <Image
-          src="/i_arrow-right.png"
-          alt="arrow"
-          width={24}
-          height={24}
-          layout="fixed"
-        />
-      </div>
+      <a
+        target="_blank"
+        href={newsletter.subscriptionLink}
+        rel="noopener noreferrer"
+      >
+        <div className={cardStyles.subscriptionBtn}>
+          구독하기
+          <Icon icon={arrowLine} rotate="90deg" />
+        </div>
+      </a>
     </div>
   );
 };
