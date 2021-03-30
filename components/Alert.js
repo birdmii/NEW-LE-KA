40ba9489d86079b653b2966c71c2903340a8e03c
jung.sammie <jung.sammie@gmail.com>
2021-03-30 21:47:40 +0900

    알림 내용 업데이트

--- components/Alert.js
+++ components/Alert.js
@@ -2,9 +2,8 @@ import alertStyles from '../styles/Alert.module.css';
 
 const Alert = () => {
   return (
-    <div className={`body-text2 shadow-1 mt-40 border-rad-8 ${alertStyles.alert}`}>
-      오픈 기념!! 좋은 피드백을 주시는 분들 께 스타벅스 기프티콘 5장을 쏩니다!!
-      ☕️☕️☕️
+    <div className={`body-text2 shadow-1 border-rad-8 ${alertStyles.alert}`}>
+      오픈 준비 중입니다! 조금만 더 기다려주세요:) 
     </div>
   );
 };
