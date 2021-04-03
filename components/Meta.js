db586d080ea67ea337bde6386bb4ce10c7788d38
jung.sammie <jung.sammie@gmail.com>
2021-04-04 00:27:02 +0900

    Google Analytics 코드 추가

--- components/Meta.js
+++ components/Meta.js
@@ -1,16 +1,32 @@
 import Head from 'next/head';
 
 const Meta = ({ title, keywords, description }) => {
+  const analytics = `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-396SQ9NZYF');`;
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
-      <meta name="google-site-verification" content="eKuO8qwY0TSHjwS20pJVPHsxlNEuUg3sJzginrPca9c" />
-      <meta name="naver-site-verification" content="0eb9c28596583cafe3efb2ce1f0ecb972080f5a7" />
+      <meta
+        name="google-site-verification"
+        content="eKuO8qwY0TSHjwS20pJVPHsxlNEuUg3sJzginrPca9c"
+      />
+      <meta
+        name="naver-site-verification"
+        content="0eb9c28596583cafe3efb2ce1f0ecb972080f5a7"
+      />
       <meta charSet="utf-8" />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-396SQ9NZYF"
+      ></script>
+      <script>{analytics}</script>
     </Head>
   );
 };
