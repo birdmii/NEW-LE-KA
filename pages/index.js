92470941783ac990cf20848e798c3781f81b0d3a
jung.sammie <jung.sammie@gmail.com>
2021-04-26 22:59:57 +0900

    Skeleton UI 적용

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,36 @@
 import Cards from '../components/Cards';
 import Alert from '../components/Alert';
+import alertStyles from '../styles/Alert.module.css';
 import MediaQuery from 'react-responsive';
+import SkeletonUI from '../components/SkeletonUI';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 export default function Home({ newsletters, alertContent, query }) {
   return (
     <div className="mt-40">
       <MediaQuery minWidth={1051}>
-        <Alert alertContent={alertContent}/>
-        <Cards category={'랜덤모두보기'} newsletters={newsletters} />
+        {alertContent ? (
+          <Alert alertContent={alertContent} />
+        ) : (
+          <Skeleton
+            variant="rect"
+            height={50}
+            className={alertStyles['Alert--skeleteon']}
+          />
+        )}
+
+        {newsletters ? (
+          <Cards category={'랜덤모두보기'} newsletters={newsletters} />
+          ) : (
+          <SkeletonUI />
+        )}
       </MediaQuery>
       <MediaQuery maxWidth={1050}>
-        <Cards category={'랜덤모두보기'} newsletters={newsletters} />
+        {newsletters ? (
+          <SkeletonUI />
+          ) : (
+          <Cards category={'랜덤모두보기'} newsletters={newsletters} />
+        )}
       </MediaQuery>
     </div>
   );
@@ -22,7 +42,7 @@ export const getServerSideProps = async () => {
   );
   const newsletters = await res.json();
 
-  const resAlert = await fetch(`https://newleka.herokuapp.com/alerts/1`,);
+  const resAlert = await fetch(`https://newleka.herokuapp.com/alerts/1`);
   const alertContent = await resAlert.json();
 
   let m = newsletters.length;
@@ -37,7 +57,7 @@ export const getServerSideProps = async () => {
   return {
     props: {
       newsletters,
-      alertContent
+      alertContent,
     },
   };
 };
