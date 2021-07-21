1b9ffb7f0ae697805fdb6ec97273405ffef7b459
jung.sammie <jung.sammie@gmail.com>
2021-07-21 13:22:21 +0900

    Newsletter list, item panel 추가 작업중

--- pages/admin.js
+++ pages/admin.js
@@ -1,26 +1,30 @@
 import AdminNav from "../components/AdminNav";
 import adminStyle from "../styles/Admin.module.css";
 import { useState, useEffect } from "react";
+import useSWR from "swr";
 import { parseCookies } from "nookies";
 import { getUser } from "./api/admin";
 import Router, { useRouter } from "next/router";
-import { editAlert, getAlert } from "./api/newsletter";
+import { editAlert, getAlert, getAllData } from "./api/newsletter";
+import Card from "../components/Card";
 
-const getAllNewsletters = async (token) => {
-  const res = await fetch("https://newleka.herokuapp.com/alerts/", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-const admin = ({ admin, token, alert }) => {
+const admin = ({ admin, token, alert, newsletters }) => {
   const router = useRouter();
   const [adminObj, setAdminObj] = useState(admin);
   const [searchText, setSearchText] = useState("");
   const [alertMsg, setAlertMsg] = useState(alert.content);
   const [isEdit, setEditMode] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const [selectedId, setNewsletterId] = useState();
+  // const [newsletterList, setNewsletterList] = useState(newsletters);
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_API_URL}newsletters?_limit=-1&_sort=id:asc`,
+    fetcher
+  );
+
+  console.log(data);
 
   useEffect(() => {
     if (!adminObj) router.push("/login");
@@ -30,7 +34,6 @@ const admin = ({ admin, token, alert }) => {
     e.preventDefault();
 
     try {
-      await getAllNewsletters(token);
     } catch (error) {}
   };
 
@@ -49,8 +52,12 @@ const admin = ({ admin, token, alert }) => {
     if (result === 200) {
       setEditMode(!isEdit);
     } else {
-      setErrorMsg('[Edit Alert Message] ⚠️ Something went wrong!!!')
+      setErrorMsg("[Edit Alert Message] ⚠️ Something went wrong!!!");
     }
+  };
+
+  const handleNewsletterItem = (e) => {
+    setNewsletterId(parseInt(e.target.id));
   };
 
   return (
@@ -60,9 +67,13 @@ const admin = ({ admin, token, alert }) => {
         <div className={`container`}>
           <div className={`flex flex-vertical-center`}>
             <h3 className={`mt-24 mb-24`}>Welcome, {adminObj.username} 🐥</h3>
-            {errorMsg&&<div className={`ml-10 ${adminStyle["errorPanel"]}`}>{errorMsg}</div>}
+            {errorMsg && (
+              <div className={`ml-10 ${adminStyle["errorPanel"]}`}>
+                {errorMsg}
+              </div>
+            )}
           </div>
-          <div className={`${adminStyle["panel"]}`}>
+          <div className={`mb-24 ${adminStyle["panel"]}`}>
             <h4 className={`subtitle bold mb-8`}>Alert Message</h4>
             <div className={`flex`}>
               {isEdit ? (
@@ -96,6 +107,44 @@ const admin = ({ admin, token, alert }) => {
               )}
             </div>
           </div>
+
+          <div className={`flex`}>
+            <div className={`mr-24 ${adminStyle["panel"]} ${adminStyle["newsletterPanel"]}`}>
+              <h4 className={`subtitle bold mb-8`}>Newsletter List</h4>
+              <ul className={`${adminStyle["newsletterList"]}`}>
+                {data
+                  ? data.map((newsletter) => (
+                      <li
+                        key={newsletter.id}
+                        className={`mb-8 ${adminStyle["newsletterItem"]}`}
+                      >
+                        <span
+                          id={newsletter.id}
+                          onClick={(e) => handleNewsletterItem(e)}
+                        >
+                          {newsletter.title}
+                        </span>
+                        <button className={`ml-10 ${adminStyle["editBtn"]}`}>
+                          Delete
+                        </button>
+                      </li>
+                    ))
+                  : "loading!!!"}
+              </ul>
+            </div>
+            <div
+              className={`${adminStyle["panel"]} ${adminStyle["newsletterPanel"]}`}
+            >
+              <h4 className={`subtitle bold mb-8`}>Newsletter Item</h4>
+              {selectedId ? (
+                <Card newsletter={data[selectedId - 1]} />
+              ) : (
+                <div className={`flex-center`}>
+                  No item has been selected
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     )
@@ -120,12 +169,14 @@ export const getServerSideProps = async (context) => {
   const admin = await getUser(context, token);
 
   const alert = await getAlert();
+  const newsletters = await getAllData();
 
   return {
     props: {
       admin: admin ? admin : null,
       token: admin ? token : null,
       alert,
+      newsletters,
     },
   };
 };
