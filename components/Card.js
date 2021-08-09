53cf61afe5274d5661703620632971113554e4b2
jung.sammie <jung.sammie@gmail.com>
2021-08-09 10:18:00 +0900

    아카이브 링크 아이콘 추가

--- components/Card.js
+++ components/Card.js
@@ -1,43 +1,45 @@
-import cardStyles from '../styles/Card.module.css';
-import { Icon } from '@iconify/react';
-import arrowLine from '@iconify/icons-clarity/arrow-line';
+import cardStyles from "../styles/Card.module.css";
+import { Icon } from "@iconify/react";
+import arrowLine from "@iconify/icons-clarity/arrow-line";
+import fileGroupLine from "@iconify/icons-clarity/file-group-line";
 
 const Card = ({ newsletter }) => {
   const categoryArr = [
-    { code: 'economy', title: '경제' },
-    { code: 'education', title: '교육' },
-    { code: 'news', title: '뉴스' },
-    { code: 'design', title: '디자인' },
-    { code: 'lifestyle', title: '라이프스타일' },
-    { code: 'marketing', title: '마케팅' },
-    { code: 'culture', title: '문화' },
-    { code: 'work', title: '일과 노동' },
-    { code: 'tech', title: '테크' },
-    { code: 'trend', title: '트렌드' },
-    { code: 'society', title: '사회'}
+    { code: "economy", title: "경제" },
+    { code: "education", title: "교육" },
+    { code: "news", title: "뉴스" },
+    { code: "design", title: "디자인" },
+    { code: "lifestyle", title: "라이프스타일" },
+    { code: "marketing", title: "마케팅" },
+    { code: "culture", title: "문화" },
+    { code: "work", title: "일과 노동" },
+    { code: "tech", title: "테크" },
+    { code: "trend", title: "트렌드" },
+    { code: "society", title: "사회" },
   ];
 
   const sendingTermArr = [
-    { code: 'daily', name: '매일' },
-    { code: 'weekly', name: '매주' },
-    { code: 'tendays', name: '10일' },
-    { code: 'biweekly', name: '격주' },
-    { code: 'monthly', name: '매달' },
-    { code: '?', name: '?' },
+    { code: "daily", name: "매일" },
+    { code: "weekly", name: "매주" },
+    { code: "tendays", name: "10일" },
+    { code: "biweekly", name: "격주" },
+    { code: "monthly", name: "매달" },
+    { code: "?", name: "?" },
   ];
 
   const sendingDayArr = [
-    { code: 'Mon', name: '월' },
-    { code: 'Tue', name: '화' },
-    { code: 'Wed', name: '수' },
-    { code: 'Thu', name: '목' },
-    { code: 'Fri', name: '금' },
-    { code: 'Sat', name: '토' },
-    { code: 'Sun', name: '일' },
+    { code: "Mon", name: "월" },
+    { code: "Tue", name: "화" },
+    { code: "Wed", name: "수" },
+    { code: "Thu", name: "목" },
+    { code: "Fri", name: "금" },
+    { code: "Sat", name: "토" },
+    { code: "Sun", name: "일" },
   ];
 
   const tagArr = newsletter.tag.tag;
   const sendingDays = newsletter.sendingday.day;
+  const sampleLink = newsletter.samplelink !== '-' ? newsletter.samplelink : '';
   let categoryTitle;
   categoryArr.forEach((item) => {
     if (newsletter.category === item.code) {
@@ -55,27 +57,40 @@ const Card = ({ newsletter }) => {
   return (
     <div className={`${cardStyles.Card} shadow-1`}>
       <div className="flex mb-8">
-        <span className={`${cardStyles['Card__category']} ${newsletter.category}`}>
+        <span
+          className={`${cardStyles["Card__category"]} ${newsletter.category}`}
+        >
           {categoryTitle}
         </span>
       </div>
-      <h2 className={cardStyles['Card__title']}>{newsletter.title}</h2>
-      <p className={`body-text2 ${cardStyles['Card__description']}`}>
+      <h2 className={cardStyles["Card__title"]}>
+        {newsletter.title}
+
+        {sampleLink && (
+          <a target="_blank" href={sampleLink} rel="noopener noreferrer">
+            <Icon className={`ml-4`} icon={fileGroupLine} style={{ fontSize: '18px'}} color='#9799a2'/>
+          </a>
+        )}
+      </h2>
+
+      <p className={`body-text2 ${cardStyles["Card__description"]}`}>
         {newsletter.description}
       </p>
 
-      <div className={cardStyles['Card__tagSection']}>
+      <div className={cardStyles["Card__tagSection"]}>
         <div className="caption">발행 주기</div>
         <div className="mt-8">
-          <span className={cardStyles['tagSection__sendingTermTag']}>{sendingTerm}</span>
+          <span className={cardStyles["tagSection__sendingTermTag"]}>
+            {sendingTerm}
+          </span>
         </div>
         <div className="mt-8">
-          {newsletter.sendingterm === 'daily'
+          {newsletter.sendingterm === "daily"
             ? sendingDayArr.map((day) => {
                 return (
                   <span
                     key={day.code}
-                    className={`${cardStyles['tagSection__sendingDayTag']} ${cardStyles.on}`}
+                    className={`${cardStyles["tagSection__sendingDayTag"]} ${cardStyles.on}`}
                   >
                     {day.name}
                   </span>
@@ -86,14 +101,19 @@ const Card = ({ newsletter }) => {
                   return (
                     <span
                       key={day.code}
-                      className={`${cardStyles['tagSection__sendingDayTag']} ${cardStyles.on}`}
+                      className={`${cardStyles["tagSection__sendingDayTag"]} ${cardStyles.on}`}
                     >
                       {day.name}
                     </span>
                   );
                 }
                 return (
-                  <span key={day.code} className={cardStyles['tagSection__sendingDayTag']}>{day.name}</span>
+                  <span
+                    key={day.code}
+                    className={cardStyles["tagSection__sendingDayTag"]}
+                  >
+                    {day.name}
+                  </span>
                 );
               })}
         </div>
@@ -105,7 +125,7 @@ const Card = ({ newsletter }) => {
               return (
                 <span
                   key={tag}
-                  className={`caption mr-8 ${cardStyles['tagSection__relatedTag']}`}
+                  className={`caption mr-8 ${cardStyles["tagSection__relatedTag"]}`}
                 >
                   {tag}
                 </span>
@@ -119,7 +139,7 @@ const Card = ({ newsletter }) => {
         href={newsletter.subscriptionlink}
         rel="noopener noreferrer"
       >
-        <div className={cardStyles['Card__subscriptionBtn']}>
+        <div className={cardStyles["Card__subscriptionBtn"]}>
           구독하기
           <Icon icon={arrowLine} rotate="90deg" />
         </div>
